8190126290115cdbf928148812c7845cf9378065
Michael Brother <mdbrother697@gmail.com>
2016-05-08 20:00:29 -0400

    Initial version with functions added
    Does not wo0rk correctly. Can not call functions in makeCachematrix
    correctly

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment 2
+## Michael Brother
 
-## Write a short comment describing this function
+## This functions creates a cached version of a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinverse <- function(solve) m <<- solve
+  
+  getinverse <- function() m
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -11,5 +26,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x["getinverse()"]
+  if(!is.null(m)){
+    message("getting cached inverse")
+    return(m)
+  }
+  
+  data <- x["getinverse()"]
+  m <- solve(data)
+  x["setinverse(m)"]
+  m
+
 }
